f363ac5151d0df723b1e23d8b2e556c666755e4c
Ishan Arya <ishan.arya130@gmail.com>
2025-05-08 14:16:05 -0400

    add sst

---
+++ sst.config.ts
@@ -0,0 +1,17 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "pluggedin",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+    };
+  },
+  async run() {
+    // const vpc = new sst.aws.Vpc("MyVpc");
+    // const database = new sst.aws.Postgres("MyDatabase", { vpc });
+    new sst.aws.React("MyWeb");
+  },
+});
