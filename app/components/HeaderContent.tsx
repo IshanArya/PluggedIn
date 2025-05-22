2c968efd3d2a849aea9af77a1c88cba8d084a4c5
Ishan Arya <ishan.arya130@gmail.com>
2025-05-22 14:02:34 -0700

    user

--- app/components/HeaderContent.tsx
+++ app/components/HeaderContent.tsx
@@ -1,8 +1,12 @@
+import { LoginButton } from './LoginButton';
 
 export function HeaderContent() {
     return (
         <div style={{ height: '100%', display: 'flex', alignItems: 'center', paddingLeft: 16, fontWeight: 600 }}>
             PluggedIn
+            <div style={{ marginLeft: 'auto', paddingRight: 16 }}>
+                <LoginButton />
+            </div>
         </div>
     );
 } 
